ea7cf42122bfb1b4ca5513535030fb2c523563ca
coderaiser <coderaiser@users.noreply.github.com>
2023-08-13 10:01:51 +0000

    chore: mock-import actions: lint ☘️

--- lib/mock-import.d.ts
+++ lib/mock-import.d.ts
@@ -21,4 +21,4 @@ type MockImport = {
 
 export declare function createMockImport(url: string): MockImport;
 export declare function enableNestedImports(): void;
-export declare function disableNestedImports(): void; 
+export declare function disableNestedImports(): void;
